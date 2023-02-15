f60e74adc2a8e5a2faecb19567764cebd7277718
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-02-15 20:13:06 -0300

    Adiciona teste da pagina login e faz alterações na estrutura para q o teste funcione

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Icon from './Pages/Icon';
 import Login from './Pages/Login';
 
 function App() {
   return (
     <main>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Icon /> } />
-          <Route path="/login" element={ <Login /> } />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Icon /> } />
+        <Route path="/login" element={ <Login /> } />
+      </Routes>
     </main>
   );
 }
---
+++ src/tests/Login.test.jsx
@@ -0,0 +1,46 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Testa a pagina de Login', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter initialEntries={ ['/login'] }>
+        <App />
+      </MemoryRouter>,
+    );
+  });
+
+  test('Se existem imagens, inputs e um botão desabilitado na pagina de login', () => {
+    const logoApp = screen.getByRole('img', { name: /logotipo do app/i });
+    const salad = screen.getByRole('img', { name: /salada com alface e tomate/i });
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const enterButton = screen.getByRole('button', { name: /enter/i });
+
+    expect(logoApp).toBeInTheDocument();
+    expect(salad).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(enterButton).toBeInTheDocument();
+    expect(enterButton).toBeDisabled();
+  });
+
+  test('Se salva no local storage e envia para /foods ao clickar no botão', () => {
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    const enterButton = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(emailInput, 'test@test.com');
+    userEvent.type(passwordInput, '1234567');
+    expect(enterButton).toBeEnabled();
+    userEvent.click(enterButton);
+
+    expect(emailInput).not.toBeInTheDocument();
+    expect(passwordInput).not.toBeInTheDocument();
+    expect(enterButton).not.toBeInTheDocument();
+  });
+});
