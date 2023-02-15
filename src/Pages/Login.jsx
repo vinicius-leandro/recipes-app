ce320e8243db2fbaf17f1f851cc475e0d78e599c
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-02-15 17:47:56 -0300

    Adiciona imagens na pagina de login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import LoginForm from '../Components/LoginForm';
+import appLogo from '../images/appLogo.svg';
+import salad from '../images/salad.svg';
 
 export default function Login() {
   return (
     <>
       <section>
         <figure>
-          <img src="" alt="" />
+          <img src={ appLogo } alt="logotipo do app" />
         </figure>
         <figure>
-          <img src="" alt="" />
+          <img src={ salad } alt="salada com alface e tomate" />
         </figure>
       </section>
       <section>
