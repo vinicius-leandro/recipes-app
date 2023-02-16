a5497718fad57b82b6637ccaf82dd953210208e2
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-02-16 14:25:13 -0300

    Adiciona form com input tipo button e um botão, adiciona tbm lógica pra saber qual botão ta ativo

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchBar() {
+  const [radioInput, setRadioInput] = useState();
+  console.log(radioInput);
+
+  const handleRadioInpt = (nameInput) => {
+    setRadioInput(nameInput);
+  };
   return (
     <section>
-      <input type="text" data-testid="search-input" />
+      <form>
+        <section>
+          <input type="text" data-testid="search-input" />
+        </section>
+        <section>
+          <label htmlFor="ingredientRadioInput">
+            <input
+              type="radio"
+              name="searchFilter"
+              id="ingredientRadioInput"
+              value="ingredient"
+              data-testid="ingredient-search-radio"
+              onClick={ ({ target: { value } }) => handleRadioInpt(value) }
+            />
+            Ingredient
+          </label>
+          <label htmlFor="nameRadioInput">
+            <input
+              type="radio"
+              name="searchFilter"
+              id="nameRadioInput"
+              value="name"
+              data-testid="name-search-radio"
+              onClick={ ({ target: { value } }) => handleRadioInpt(value) }
+            />
+            Name
+          </label>
+          <label htmlFor="firstLetterRadioInput">
+            <input
+              type="radio"
+              name="searchFilter"
+              id="firstLetterRadioInput"
+              value="firstLetter"
+              data-testid="first-letter-search-radio"
+              onClick={ ({ target: { value } }) => handleRadioInpt(value) }
+            />
+            First Letter
+          </label>
+        </section>
+        <section>
+          <button type="button" data-testid="exec-search-btn">
+            SEARCH
+          </button>
+        </section>
+      </form>
     </section>
   );
 }
