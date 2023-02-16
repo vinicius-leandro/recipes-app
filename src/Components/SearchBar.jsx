3a395c64b19b89738f0ca2e0bcf6f15d1c06f24e
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-02-16 18:15:07 -0300

    Adiciona função que lida com a chamada a requisição

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import {
+  getRecipesByFirstLetter,
+  getRecipesByIngredients,
+  getRecipesByName,
+} from '../Service/requests';
 
 export default function SearchBar() {
+  const { pathname } = useLocation();
+  const [filterSearch, setFilterSearch] = useState('');
   const [radioInput, setRadioInput] = useState();
-  console.log(radioInput);
 
-  const handleRadioInpt = (nameInput) => {
-    setRadioInput(nameInput);
+  const handleRequest = async () => {
+    const apiUrl = pathname.includes('food') ? 'food' : 'drink';
+
+    const filters = {
+      ingredient: (choice, ingredient) => getRecipesByIngredients(choice, ingredient),
+      name: (choice, name) => getRecipesByName(choice, name),
+      firstLetter: (choice, firstLetter) => getRecipesByFirstLetter(choice, firstLetter),
+    };
+
+    if (radioInput === 'firstLetter' && filterSearch.length !== 1) {
+      global.alert('Your search must have only 1 (one) character');
+    }
+
+    const result = await filters[radioInput](apiUrl, filterSearch);
+    console.log(result);
   };
 
   return (
     <section>
       <form>
         <section>
-          <input type="text" data-testid="search-input" />
+          <input
+            type="text"
+            data-testid="search-input"
+            onChange={ ({ target: { value } }) => setFilterSearch(value) }
+          />
         </section>
         <section>
           <label htmlFor="ingredientRadioInput">
@@ -22,7 +46,7 @@ export default function SearchBar() {
               id="ingredientRadioInput"
               value="ingredient"
               data-testid="ingredient-search-radio"
-              onClick={ ({ target: { value } }) => handleRadioInpt(value) }
+              onClick={ ({ target: { value } }) => setRadioInput(value) }
             />
             Ingredient
           </label>
@@ -33,7 +57,7 @@ export default function SearchBar() {
               id="nameRadioInput"
               value="name"
               data-testid="name-search-radio"
-              onClick={ ({ target: { value } }) => handleRadioInpt(value) }
+              onClick={ ({ target: { value } }) => setRadioInput(value) }
             />
             Name
           </label>
@@ -44,13 +68,13 @@ export default function SearchBar() {
               id="firstLetterRadioInput"
               value="firstLetter"
               data-testid="first-letter-search-radio"
-              onClick={ ({ target: { value } }) => handleRadioInpt(value) }
+              onClick={ ({ target: { value } }) => setRadioInput(value) }
             />
             First Letter
           </label>
         </section>
         <section>
-          <button type="button" data-testid="exec-search-btn">
+          <button type="button" data-testid="exec-search-btn" onClick={ handleRequest }>
             SEARCH
           </button>
         </section>
