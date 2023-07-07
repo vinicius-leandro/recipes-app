a9d05c8ae31c07194cb0644965c431128a3a972c
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-07-07 17:28:06 -0300

    Coloca provider no index.js

--- src/Pages/Recipes.jsx
+++ src/Pages/Recipes.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Header from '../Components/Header';
-import { RecipesProvider } from '../Context/RecipesContext';
 
 export default function Recipes() {
   return (
     <div>
-      <RecipesProvider>
-        <Header />
-      </RecipesProvider>
+      <Header />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { RecipesProvider } from './Context/RecipesContext';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <RecipesProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecipesProvider>,
   document.getElementById('root'),
 );
