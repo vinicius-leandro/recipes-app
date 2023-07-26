78be781aeaaddd16dad6bb33e5735680d1087739
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-07-26 18:03:49 -0300

    instala styled-components e suas configurações basicas

---
+++ src/Styles/GlobalStyles.styled.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  body {
+    font-family: 'Epilogue', sans-serif;
+  }
+`;
+
+export default GlobalStyle;
