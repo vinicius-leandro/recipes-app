74fc240118cf1f11f1005c99674241b90a57cb34
Vinicius Leandro <viniciusleandrodev@gmail.com>
2023-07-28 18:09:55 -0300

    Estilização footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import cupIcon from '../images/cupIcon.svg';
 import dishIcon from '../images/dishIcon.svg';
+import FooterContainer from '../Styles/Components/Footer.styled';
 
 export default function Footer() {
   return (
-    <footer data-testid="footer">
+    <FooterContainer>
       <Link to="/drinks">
         <figure>
           <img src={ cupIcon } alt="icone de copo de drink" />
@@ -16,6 +17,6 @@ export default function Footer() {
           <img src={ dishIcon } alt="icone de prato com garfo e faca" />
         </figure>
       </Link>
-    </footer>
+    </FooterContainer>
   );
 }
